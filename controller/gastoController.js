c181da426ddc4ebbcf0adbbbee061b3fe5030588
patorma <pocontreras@ing.ucsc.cl>
2019-11-13 02:02:17 -0300

    se creo archivo controlador, el archivo modelo degasto, la ruta de gasto y se modifico archivo server

---
+++ controller/gastoController.js
@@ -0,0 +1,5 @@
+const Gasto = require('../models/Gasto')
+const { check, validationResult } = require('express-validator');
+async function addGasto(req, res) {
+
+}
---
+++ db/mongodb.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+async function connectDb({ host, port, dbName }) {
+    //se destructura para obtener informacion de variables disponible
+    const url = `mongodb://${host}:${port}/${dbName}`
+    await mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false })
+}
+
+module.exports = connectDb

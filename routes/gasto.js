4398830cadba91b26cb2e46e138d9feea4e05df7
patorma <pocontreras@ing.ucsc.cl>
2019-11-13 20:31:53 -0300

    se hizo ruta degasto

--- routes/gasto.js
+++ routes/gasto.js
@@ -1,3 +1,8 @@
 const express = require('express')
-const { check, validationResult } = require('express-validator');
+const { check } = require('express-validator');
+const { addGasto } = require('../controller/gastoController')
 const api = express.Router()
+
+api.post('/gastos', [check('tipoGasto').isLength({ min: 2 }), check('descripcion').isLength({ min: 10 })], addGasto)
+
+module.exports = api
